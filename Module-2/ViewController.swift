ac590fc21e728a328de40accabbf5129bb4c28ba
David Montes <david.montes@sjsu.edu>
2019-12-31 15:56:22 -0800

    Initial Commit

---
+++ Module-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Module-2
+//
+//  Created by David Montes on 12/31/19.
+//  Copyright © 2019 LearnAppMaking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
